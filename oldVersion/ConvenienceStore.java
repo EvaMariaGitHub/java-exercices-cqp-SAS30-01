7691d9062129baeb4d181c04ca720fe86364f99c
EvaMariaGitHub <alonso.eva.ea@gmail.com>
2017-02-08 21:42:47 +0100

    ajout old version LifeStores

---
+++ oldVersion/ConvenienceStore.java
@@ -0,0 +1,10 @@
+package com.simplon;
+
+/**
+ * Created by alonso on 07/02/17.
+ */
+public interface ConvenienceStore {
+
+    public Boolean sell(Product product);
+    public Boolean filled();
+}
